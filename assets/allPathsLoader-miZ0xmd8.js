4f238ff8dc7cc6fb54f3d6c26d5ad55db41a2b4a
Hi Sir... <kathikmrn@gmail.com>
2024-01-23 10:41:20 +0530

    Updates

---
+++ assets/allPathsLoader-miZ0xmd8.js
@@ -0,0 +1,7 @@
+import{_ as o,a as n,b as i}from"./index-d8V0McYi.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-LD4cj6mO.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-LD4cj6mO.js","assets/index-6QKa2Ynx.js","assets/index-s_3b95Qx.js","assets/index-d8V0McYi.js","assets/index-eVEqRChb.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
